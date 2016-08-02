30b4029b067ae7fad1fab5c9714e00ea9d69e7a6
함기현 <coz93@naver.com>
2016-08-02 09:40:51 +0900

    로그인,

--- metour/src/main/java/com/kitri/meto/mainsub/MainSubPlanMapper.java
+++
@@ -1,9 +0,0 @@
-package com.kitri.meto.mainsub;
-
-import java.util.ArrayList;
-
-import com.kitri.meto.JoinDTO.JoinDTO;
-
-public interface MainSubPlanMapper {
-	ArrayList<JoinDTO> selectByMainNum(int main_num);
-}
--- metour/src/main/java/com/kitri/meto/mainsub/MainSubPlanService.java
+++
@@ -1,9 +0,0 @@
-package com.kitri.meto.mainsub;
-
-import java.util.ArrayList;
-
-import com.kitri.meto.JoinDTO.JoinDTO;
-
-public interface MainSubPlanService {
-	ArrayList<JoinDTO> getMainSubPlanByMainNum(int main_num);
-}
