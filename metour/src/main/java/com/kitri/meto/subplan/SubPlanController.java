10c6c53a87092870e0a5a6b7a51d5e842a27687d
heesong66 <heesong66@naver.com>
2016-08-03 13:20:09 +0900

    세부계획 리스트 변경

--- metour/src/main/java/com/kitri/meto/subplan/SubPlanController.java
+++ metour/src/main/java/com/kitri/meto/subplan/SubPlanController.java
@@ -24,54 +24,8 @@ public class SubPlanController {
 	public ModelAndView subPlanAdd(){
 		ModelAndView mav = new ModelAndView("subplan/subPlanAdd");
 		
-		//시간중복
 		ArrayList<SubPlan> sub = subPlanService.getSubPlans(1);
-		
-		ArrayList<String> start = new ArrayList<String>();
-		ArrayList<String> end = new ArrayList<String>();
-		
-		for(int i = 0; i < sub.size(); i++){
-			start.add(sub.get(i).getStart_time());
-			end.add(sub.get(i).getEnd_time());
-		}
-		
-		ArrayList<Integer> index = new ArrayList<Integer>();
-		
-		for(int i = 0; i < start.size(); i++){
-			int start_i = getIndex(start.get(i).substring(2, 4), start.get(i).substring(5, 7), start.get(i).substring(0, 2));
-			int end_i = getIndex(end.get(i).substring(2, 4), end.get(i).substring(5, 7), end.get(i).substring(0, 2));
-			
-			int size = 0;
-			if(start_i > end_i){
-				size = start_i - end_i + 1;
-				for(int j = 0 ; j < size ; j++){
-					index.add(end_i++);
-				}
-			}else{
-				size = end_i - start_i + 1;
-				for(int j = 0 ; j < size ; j++){
-					index.add(start_i++);
-				}
-			}
-		}
-		
-		//중복제거
-		HashSet hs = new HashSet(index);
-		ArrayList<Integer> newindex = new ArrayList<Integer>(hs);
-		
-		ArrayList<Integer> flag = new ArrayList<Integer>(); 
-		for(int i = 0; i < 48; i++){
-			int tmp = 1;
-			for(int j = 0; j < newindex.size(); j++){
-				if(i == newindex.get(j)){
-					tmp = 0;
-				}
-			}
-			
-			flag.add(tmp);
-			tmp = 1;
-		}
-		
+		ArrayList<Integer> flag = getFlag(sub);
 		mav.addObject("index", flag);
 		return mav;
 	}
@@ -97,24 +51,7 @@ public class SubPlanController {
 		
 	}
 	
-	@RequestMapping(value = "/subplan/list.do")
-	public ModelAndView subPlanList(HttpServletRequest request){
-		ModelAndView mav = new ModelAndView("subplan/subPlanList");
-		mav.addObject("list", subPlanService.getSubPlans(1)); 
-		return mav;
-	}
-	
-	@RequestMapping(value = "/subplan/listview.do")
-	public ModelAndView subPlanList_detail(HttpServletRequest request, @RequestParam(value="sub_num")int subNum){
-		SubPlan subplan = subPlanService.getSubPlan(subNum);
-		
-		ModelAndView mav = new ModelAndView("subplan/subPlanDetail");
-		mav.addObject("subplan", subplan); 
-		
-		
-		//시간중복
-		ArrayList<SubPlan> sub = subPlanService.getSubPlans(1);
-		
+	public ArrayList<Integer> getFlag(ArrayList<SubPlan> sub){
 		ArrayList<String> start = new ArrayList<String>();
 		ArrayList<String> end = new ArrayList<String>();
 		
@@ -131,12 +68,12 @@ public class SubPlanController {
 			
 			int size = 0;
 			if(start_i > end_i){
-				size = start_i - end_i + 1;
+				size = start_i - end_i;
 				for(int j = 0 ; j < size ; j++){
 					index.add(end_i++);
 				}
 			}else{
-				size = end_i - start_i + 1;
+				size = end_i - start_i;
 				for(int j = 0 ; j < size ; j++){
 					index.add(start_i++);
 				}
@@ -144,7 +81,7 @@ public class SubPlanController {
 		}
 		
 		//중복제거
-		HashSet hs = new HashSet(index);
+		HashSet<Integer> hs = new HashSet<Integer>(index);
 		ArrayList<Integer> newindex = new ArrayList<Integer>(hs);
 		
 		ArrayList<Integer> flag = new ArrayList<Integer>(); 
@@ -159,6 +96,115 @@ public class SubPlanController {
 			flag.add(tmp);
 			tmp = 1;
 		}
+		return flag;
+	}
+	
+	@RequestMapping(value = "/subplan/list.do")
+	public ModelAndView subPlanList(HttpServletRequest request){
+		ModelAndView mav = new ModelAndView("subplan/subPlanList");
+		ArrayList<SubPlan> sub = subPlanService.getSubPlans(1);
+		
+		ArrayList<String> time = getTime();
+		mav.addObject("time", time);
+		
+		ArrayList<SubPlanList> title = getTitlebyTime(sub);
+		mav.addObject("splist", title);
+		return mav;
+	}
+	
+	public ArrayList<String> getTime(){
+		ArrayList<String> time = new ArrayList<String>();
+		
+		int hour = 0;
+		boolean sw = true;
+		for(int i = 0; i < 48; i++){
+			String tmp = null;
+			if(hour <= 9){
+				tmp = "0";
+			}else{
+				tmp = "";
+			}
+			tmp += hour + ":30";
+			
+			if(sw){
+				sw = false;
+				String add = null;
+				if(hour <= 9){
+					add = "0";
+				}else{
+					add = "";
+				}
+				
+				add += hour + ":00~" + tmp;
+				time.add(add);
+			}else{
+				sw = true;
+				hour++;
+				String add = tmp + "~";
+				
+				if(hour <= 9){
+					add += "0";
+				}else{
+					add += "";
+				}
+				
+				add += hour + ":00";
+				
+				time.add(add);
+			}
+		}
+		
+		return time;
+	}
+	
+	public ArrayList<SubPlanList> getTitlebyTime(ArrayList<SubPlan> sub){
+		ArrayList<String> start = new ArrayList<String>();
+		ArrayList<String> end = new ArrayList<String>();
+		
+		for(int i = 0; i < sub.size(); i++){
+			start.add(sub.get(i).getStart_time());
+			end.add(sub.get(i).getEnd_time());
+		}
+		
+		String start_now[] = new String[sub.size()];
+		String end_now[] = new String[sub.size()];
+		for(int i = 0; i < sub.size(); i++){
+			SubPlan time = subPlanService.getSupPlanTime(sub.get(i).getSub_num());
+			start_now[i] = time.getStart_time();
+			end_now[i] = time.getEnd_time();
+		}
+		
+		ArrayList<SubPlanList> title = new ArrayList<SubPlanList>();
+		for(int i = 0; i < 48; i++){
+			SubPlanList sp = new SubPlanList(0,"","","","");
+			title.add(sp);
+		}
+		
+		for(int j = 0; j < start_now.length; j++){
+			int start_i = getIndex(start_now[j].substring(2, 4), start_now[j].substring(3, 5), start_now[j].substring(0, 2));
+			int end_i = getIndex(end_now[j].substring(2, 4), end_now[j].substring(5, 7), end_now[j].substring(0, 2));
+			for(int i = start_i; i < end_i; i++){
+				SubPlanList sp = new SubPlanList();
+				sp.setSub_num(sub.get(j).getSub_num());
+				sp.setTitle(sub.get(j).getSub_title());
+				sp.setPlace(sub.get(j).getPlace());
+				sp.setMission(sub.get(j).getMission());
+				sp.setMission_yn(sub.get(j).getMission_yn());
+				title.set(i, sp);
+			}
+		}
+		
+		return title;
+		
+	}
+	@RequestMapping(value = "/subplan/listview.do")
+	public ModelAndView subPlanList_detail(HttpServletRequest request, @RequestParam(value="sub_num")int subNum){
+		SubPlan subplan = subPlanService.getSubPlan(subNum);
+		
+		ModelAndView mav = new ModelAndView("subplan/subPlanDetail");
+		mav.addObject("subplan", subplan); 
+		ArrayList<SubPlan> sub = subPlanService.getSubPlans(1);
+		ArrayList<Integer> flag = getFlag(sub);
 		
 		//현재시간은 띄우기
 		SubPlan time = subPlanService.getSupPlanTime(subNum);
@@ -172,10 +218,6 @@ public class SubPlanController {
 		
 		for(int i = start_i; i <= end_i; i++){
 			flag.set(i, 1);
-		}
-		
-		for(int i = 0; i < flag.size(); i++){
-			System.out.println(i + ":" + flag.get(i));
 		}
 		
 		mav.addObject("index", flag);
