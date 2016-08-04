56e538b4e4f531687ddfeefa783c3576edca77a5
mhee <mhee0410@naver.com>
2016-08-04 09:07:48 +0900

    share_plan

--- metour/src/main/java/com/kitri/meto/shareplan/SharePlanMapper.java
+++ metour/src/main/java/com/kitri/meto/shareplan/SharePlanMapper.java
@@ -2,10 +2,15 @@ package com.kitri.meto.shareplan;
 
 import java.util.ArrayList;
 
+import com.kitri.meto.JoinDTO.JoinDTO;
+
 public interface SharePlanMapper {
 	void insert(SharePlan s);
 	void metooPlus(int share_num);
 	void metooMinus(int share_num);
 	SharePlan select(int share_num);
 	ArrayList<SharePlan> selectAll();
+	ArrayList<SharePlan> selectByTitle(String tmp);
+	ArrayList<JoinDTO> selectByGender(String gender);
+	ArrayList<SharePlan> selectByWriter(int writer);
 }
