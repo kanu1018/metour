c334e159943d935455763d02deb03b7da15cfb9d
kanu <rlaeodid1018@naver.com>
2016-08-16 17:36:24 +0900

    푸시알람구현중 커밋

--- metour/src/main/java/com/kitri/meto/subplan/SubPlanService.java
+++ metour/src/main/java/com/kitri/meto/subplan/SubPlanService.java
@@ -2,6 +2,8 @@ package com.kitri.meto.subplan;
 
 import java.util.ArrayList;
 
+import com.kitri.meto.schedule.Schedule;
+
 public interface SubPlanService {
 	void addSubPlan(SubPlan sp);
 	ArrayList<SubPlan> getSubPlans(int mainPlanNum);
@@ -10,4 +12,5 @@ public interface SubPlanService {
 	void editSubPlan(SubPlan sp);
 	SubPlan getSupPlanTime(int subNum);
 	void updatePhoto(SubPlan subplan);
+	ArrayList<SubPlan> getSubPlanByMainplan(Schedule s);
 }
